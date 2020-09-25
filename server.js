64e1e9e0c2fa3b12a6b211714d536cad113c04a2
rartan <ramani.artan@gmail.com>
2020-09-25 02:56:48 +0200

    server,js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -13,14 +12,14 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'client/public/index.html'));
   });
 }
 
@@ -29,9 +28,7 @@ app.listen(port, error => {
   console.log('Server running on port ' + port);
 });
 
-app.get('/service-worker.js', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
-});
+app.get("/", (req, res) => res.status(200).send("server is up and running"))
 
 app.post('/payment', (req, res) => {
   const body = {
